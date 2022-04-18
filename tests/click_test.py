b88cd93de4b9b4145ea9cfa5571bf4518bf65057
Parth Panara <parthpanara9@gmail.com>
2022-04-18 17:57:59 -0400

    updated tests

--- tests/click_test.py
+++ tests/click_test.py
@@ -15,7 +15,7 @@ def test_create_log_folder():
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
     # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '/home/myuser/app/logs')
+    logdir = os.path.join(root, '../app/logs')
     # make a directory if it doesn't exist
     assert os.path.exists(logdir) == True
 
